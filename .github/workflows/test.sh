b50dcd192043a07c195ca8c784293442d7b4a67f
Linwei Ye <bellfahle@outlook.com>
2020-11-19 09:44:43 +0800

    Create test.sh

---
+++ .github/workflows/test.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+export LOG_FAIL_PATH="log/Test-*.xml"
+
+for LOG_FILE_NAME in `ls $LOG_FAIL_PATH`
+do
+  export FAILURE_MESSAGE=`xpath -e '//testsuite/testcase/failure/text()' LOG_FILE_NAME | sed ":a;N;s/\n//g;ta"`
+  if [ ! -z "$FAILURE_MESSAGE" ]; then 
+    cat $LOG_FILE_NAME; 
+    cat $FAILURE_MESSAGE;
+    export FAILURE_MESSAGE_FILE=`xpath -e '//testsuite/testcase/@classname' LOG_FILE_NAME
+    cat $FAILURE_MESSAGE_FILE
+  else 
+    echo "nothing in $LOG_FILE_NAME";
+  fi
+
+  export ERROR_MESSAGE=`xpath -e '//testsuite/testcase/system-out/text()' log/TEST-*.xml | grep "Annotation Error Message"`
+  if [ ! -z "$ERROR_MESSAGE" ]; then 
+    cat $LOG_FILE_NAME; 
+    cat $ERROR_MESSAGE;
+    export ERROR_MESSAGE_FILE=`xpath -e '//testsuite/testcase/@classname' LOG_FILE_NAME
+    cat $ERROR_MESSAGE_FILE
+  else 
+    echo "nothing in $LOG_FILE_NAME";
+  fi
+  
+  export WARNING_MESSAGE=`xpath -e '//testsuite/testcase/system-out/text()' log/TEST-*.xml | grep "Annotation Warning Message"`
+  if [ ! -z "$WARNING_MESSAGE" ]; then 
+    cat $LOG_FILE_NAME; 
+    cat $WARNING_MESSAGE;
+    export WARNING_MESSAGE_FILE=`xpath -e '//testsuite/testcase/@classname' LOG_FILE_NAME
+    cat $WARNING_MESSAGE_FILE
+  else 
+    echo "nothing in $LOG_FILE_NAME";
+  fi
+done
